bafe69f8f09960b5d9757002415c885d4d1cbaf3
Danilo Silva <danilo.silva@msn.com>
2021-10-24 20:02:12 -0300

    Criação e Implementação da Classe Contexto

---
+++ Projeto/Projeto/Contexts/Contexto.cs
@@ -0,0 +1,13 @@
+﻿using Projeto.Models;
+using System.Data.Entity;
+
+namespace Projeto.Contexts
+{
+    public class Contexto : DbContext
+    {
+        public Contexto() : base("Asp_Net_MVC_5") {}
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Fabricante> Fabricantes { get; set; }
+    }
+}
