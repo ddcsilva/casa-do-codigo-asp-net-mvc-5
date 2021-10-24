5206732c9834edc056553fab24fb1e4df5edde92
Danilo Silva <danilo.silva@msn.com>
2021-10-24 18:11:19 -0300

    Criando CategoriasController

---
+++ Projeto/Projeto/Controllers/CategoriasController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+
+namespace Projeto.Controllers
+{
+
+    /// <summary>
+    /// 1. Toda classe Controller deve ter seu nome finalizado com "Controller"
+    /// 2. As actions devem ser public
+    /// 3. As actions não podem ser static
+    /// 4. As actions não podem ser sobrecarregadas com parâmetros, apenas com Attributes
+    /// </summary>
+
+    public class CategoriasController : Controller
+    {
+        // GET: Categorias
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
