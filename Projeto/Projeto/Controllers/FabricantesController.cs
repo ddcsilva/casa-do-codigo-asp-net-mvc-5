b4cb10eff0c87c4fc6b25e3e79103be543f24f93
Danilo Silva <danilo.silva@msn.com>
2021-10-24 22:51:31 -0300

    Criação e Implementação da FabricantesController

---
+++ Projeto/Projeto/Controllers/FabricantesController.cs
@@ -0,0 +1,17 @@
+﻿using Projeto.Contexts;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Projeto.Controllers
+{
+    public class FabricantesController : Controller
+    {
+        private Contexto contexto = new Contexto();
+
+        // GET: Fabricantes
+        public ActionResult Index()
+        {
+            return View(contexto.Fabricantes.OrderBy(f => f.Nome));
+        }
+    }
+}
