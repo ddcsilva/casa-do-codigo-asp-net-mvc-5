a397bd1b2d3395c6c99d5ab9b994aed848acaa58
Danilo Silva <danilo.silva@msn.com>
2021-10-26 15:15:35 -0300

    Criação da Controller de Produtos

---
+++ Projeto/Projeto/Controllers/ProdutosController.cs
@@ -0,0 +1,89 @@
+﻿using Projeto.Contexts;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Projeto.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        private Contexto contexto = new Contexto();
+
+        // GET: Produtos
+        public ActionResult Index()
+        {
+            return View(contexto.Produtos.OrderBy(p => p.Nome));
+        }
+
+        // GET: Produtos/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Produtos/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Produtos/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Produtos/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Produtos/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Produtos/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Produtos/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
